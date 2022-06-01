79c33d07891696c8b9fb5ebf78a512dbab746168
Julian Carlson <Jcarlson@Dev-10.com>
2022-06-01 16:12:42 -0400

    attempting to get aws working

--- server/src/main/java/capstone/controllers/HealthCheckController.java
+++ server/src/main/java/capstone/controllers/HealthCheckController.java
@@ -1,5 +1,6 @@
 package capstone.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
